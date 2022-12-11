641b6e19279106b1da292941db8393281c8f6c66
Dale Hagglund <dale.hagglund@gmail.com>
2022-12-10 23:16:27 -0700

    day 11: part 1 complete

--- day11/11.py
+++ day11/11.py
@@ -1,6 +1,6 @@
 import sys
 import typing as ty
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from enum import Enum
 from functools import partial
 import collections
@@ -77,10 +77,104 @@ def takeuntil(predicate, items):
         if predicate(item):
             break
 
+@dataclass
+class Monkey:
+    id: int = -1
+    items: list[int] = field(default_factory=list)
+    update: ty.Callable[[int], int] = lambda x: x
+    test: ty.Callable[[int], bool] = lambda x: True
+    throw_to: dict[bool, int] = field(default_factory=dict)
+    inspections: int = 0
+    
+    def process_items(self):
+        #print('Monkey', self.id)
+        items, self.items = self.items, []
+        for worry in items:
+            self.inspections += 1
+            #print('   worry', worry) 
+            worry = self.update(worry)
+            worry = worry // 3
+            #print('      updated', worry)
+            outcome = self.test(worry)
+            #print('      outcome', outcome)
+            target = self.throw_to[outcome]
+            #print('      throw to', target.id)
+            self.throw_to[outcome].receive(worry)
+        
+    def receive(self, worry):
+        self.items.append(worry)
+        
+def convert_ints(items: list[str]) -> list:
+    return [
+        int(item) if re.match("^[0-9]*$", item) else item
+        for item in items
+    ]
+
+def parse_monkey(monkeys: list[Monkey], lines: list[str]):
+    def divby(factor):
+        return lambda w: w % factor == 0
+    def update(action):
+        match action:
+            case ["*", "old"]: return lambda old: old * old
+            case ["*", n]: return lambda old: old * n
+            case ["+", "old"]: return lambda old: old + old
+            case ["+", n]: return lambda old: old + n
+            case _:
+                raise ValueError(f'unexpected action: {action}')
+
+    s = iter(lines)
+    s = map(partial(re.sub, "[:,]", ""), s)
+    s = map(str.split, s)
+    s = map(convert_ints, s)
+    #s = observe(print, s)  
+    
+    for fields in s:
+        match fields:
+            case ["Monkey", ident]:
+                monkey = monkeys[ident]
+                monkey.id = ident
+            case ["Starting", "items", *worries]:
+                for w in worries: monkey.receive(w)
+            case ["Operation", "new", "=", "old", *action]:
+                monkey.update = update(action)
+            case ["Test", "divisible", "by", factor]:
+                monkey.test = divby(factor)
+            case ["If", "true", "throw", "to", "monkey", target]:
+                monkey.throw_to[True] = monkeys[target]
+            case ["If", "false", "throw", "to", "monkey", target]:
+                monkey.throw_to[False] = monkeys[target]
+            case _:
+                raise ValueError(
+                    f'unrecognized line: {fields}'
+                )
+            
+def read_monkeys(sections: list[list[str]]) -> list[Monkey]:
+    monkeys = [ Monkey() for _ in range(len(sections)) ]
+    for i, section in enumerate(sections):
+        parse_monkey(monkeys, section)
+    return monkeys
+
+def update_round(n: int, monkeys: list[Monkey]):
+    for m in monkeys:
+        m.process_items()
+    print(f'round {n}')
+    for m in monkeys:
+        print(f'    monkey {m.id}: {m.items}')
 def part1(fname: str):
     with open(fname) as f:
         sections = read_sections(f)
     print(f'*** part 1 ***')
+
+    monkeys = read_monkeys(sections)
+    for i in range(20):
+        update_round(i + 1, monkeys)
+ 
+    counts = [m.inspections for m in monkeys]
+    print(f'{counts = }')
+    m1, m2 = sorted(counts)[-2:]
+    print(m1, m2)
+    monkey_business = m1 * m2
+    print(f'    {monkey_business = }')
 
 def part2(fname: str):
     with open(fname) as f:
